f311ce224b165695ba7ac0b6ae0cc066fdaf4531
anilkumar chigilipalli <anilkumarchigilipalli18@gmail.com>
2025-09-21 23:52:44 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=kiran
+PERSON2=anil
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning Shell scripting $PERSON1"
